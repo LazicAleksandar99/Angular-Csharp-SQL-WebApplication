226f475c8306cefcffdc9c00b675a3f86d5eccc5
Lazic99H <80058161+Lazic99H@users.noreply.github.com>
2022-07-01 17:10:46 +0200

    Folder structure,Components,Bootstrap
    Added folders from angular convention (Services,Models,Components).Added few components to project (Navbar,Login and Registration component). Also included bootstrap in to the project.

--- Frontend/src/app/app-routing.module.ts
+++ Frontend/src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './components/user/login/login.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'', redirectTo:'/user/login',pathMatch:'full'},
+  {
+    path:'user',
+    children:[
+      { path: 'login',component: LoginComponent}
+    ]
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- Frontend/src/app/app.module.ts
+++ Frontend/src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavigationBarComponent } from './components/navigation-bar/navigation-bar.component';
+import { LoginComponent } from './components/user/login/login.component';
+import { RegistrationComponent } from './components/user/registration/registration.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavigationBarComponent,
+    LoginComponent,
+    RegistrationComponent
   ],
   imports: [
     BrowserModule,
