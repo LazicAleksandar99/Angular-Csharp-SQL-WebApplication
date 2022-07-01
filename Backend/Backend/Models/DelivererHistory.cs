ce7b24e39c9ba0369c7251c6d66eb37bfb215f3d
Lazic99H <80058161+Lazic99H@users.noreply.github.com>
2022-07-01 15:52:45 +0200

    History Models
    Added 2 new models for user and deliverer history

---
+++ Backend/Backend/Models/DelivererHistory.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Backend.Models
+{
+    public class DelivererHistory
+    {
+        public long Id { get; set; }
+
+        public long DelivererId { get; set; }
+
+        public Deliverer Deliverer { get; set; }
+
+        public List<Order> AllOrders { get; set; }
+    }
+}
---
+++ Backend/Backend/Models/UserHistory.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Backend.Models
+{
+    public class UserHistory
+    {
+        public long Id { get; set; }
+        
+        public long UserId { get; set; }
+
+        public User User { get; set; }
+
+        public List<Order> AllOrders { get; set; }
+    }
+}
