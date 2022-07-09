e133ee93491224ecfc23cb65ae61e0fe618efab4
Lazic99H <80058161+Lazic99H@users.noreply.github.com>
2022-07-09 18:21:44 +0200

    Interface updated
    Updated navbar and hamburger menu

--- Frontend/src/app/components/dashboard/dashboard.component.ts
+++ Frontend/src/app/components/dashboard/dashboard.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { NavigationBarComponent } from '../navigation-bar/navigation-bar.component';
 
 @Component({
   selector: 'app-dashboard',
